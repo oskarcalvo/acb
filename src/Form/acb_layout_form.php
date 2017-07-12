6aaeb494c9d4bc1e50a78b4e418e12be506d8190
Oskar Calvo <calvo.oskar@gmail.com>
2017-07-12 09:48:46 +0200

    Add the button add another block, it is wip

--- src/Form/acb_layout_form.php
+++ src/Form/acb_layout_form.php
@@ -10,7 +10,7 @@ function acb_layout_form($form,$form_state,$objet = NULL, $objet = NULL,
 
   $list_themes = list_themes();
 
-  $regions = array_map(function($theme){
+  $theme_regions = array_map(function($theme){
     return system_region_list($theme->name);
   }, $list_themes);
 
@@ -21,7 +21,7 @@ function acb_layout_form($form,$form_state,$objet = NULL, $objet = NULL,
     '#default_value' => '',
     '#size' => 60,
     '#maxlength' => 128,
-    '#required' => TRUE,
+    //'#required' => TRUE,
     //'#element_validate' => ['_acb_validate_url'],
   ];
 
@@ -30,16 +30,16 @@ function acb_layout_form($form,$form_state,$objet = NULL, $objet = NULL,
     '#title' => 'wadus',
   ];
 
-  foreach ($regions as $region_name => $region_data) {
+  foreach ($theme_regions as $theme_name => $region_data) {
 
-    $form['theme'][$region_name] = [
+    $form['theme'][$theme_name] = [
       '#type' => 'fieldset',
-      '#title' => $region_name,
+      '#title' => $theme_name,
       '#collapsible' => TRUE,
     ];
 
     foreach ($region_data as $machine_name => $human_name) {
-      $form['theme'][$region_name][$machine_name] = [
+      $form['theme'][$theme_name][$machine_name] = [
         '#type' => 'fieldset',
         '#title' => $human_name,
         '#collapsible' => TRUE,
@@ -47,11 +47,21 @@ function acb_layout_form($form,$form_state,$objet = NULL, $objet = NULL,
         '#description' => t('Set the blocks for this region.'),
       ];
 
-      $form['theme'][$region_name][$machine_name]['field'] = [
+      $form['theme'][$theme_name][$machine_name]['block'] = [
         '#type' => 'textfield',
         '#title' => 'Block',
         '#autocomplete_path' => 'acb/acb_load_block_autocomplete_callback'
       ];
+
+      $form['theme'][$theme_name][$machine_name]['add_item'] = array(
+        '#type' => 'submit',
+        '#value' => t("Add another block in $theme_name - $machine_name"),
+        '#submit' => ['acb_layout_form_add_item'],
+        '#attributes' => [
+          'theme' => [$theme_name],
+          'region' => [$machine_name]
+        ],
+      );
 
 
     }
@@ -60,4 +70,23 @@ function acb_layout_form($form,$form_state,$objet = NULL, $objet = NULL,
   $form['submit'] = array('#type' => 'submit', '#value' => t('Save'));
 
   return $form;
+}
+
+
+function acb_layout_form_add_item($form, &$form_state) {
+
+  $region = '';
+  $theme = '';
+
+  if(isset($form_state['clicked_button']['#attributes']['region'][0])) {
+    $region = $form_state['clicked_button']['#attributes']['region'][0];
+  }
+
+  if(isset($form_state['clicked_button']['#attributes']['theme'][0])){
+    $theme = $form_state['clicked_button']['#attributes']['theme'][0];
+  }
+  if(!empty($region) && !empty($theme)) {
+    $form_state['theme'][$theme][$region]['block']['number']++;
+    $form_state['rebuild'] = TRUE;
+  }
 }
