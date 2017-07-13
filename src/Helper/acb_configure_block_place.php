9b462e5f360fbc78e7ff96e25aee4ce1628e7ef7
Oskar Calvo <calvo.oskar@gmail.com>
2017-07-13 13:47:19 +0200

    Load teh file acb_load_block in the file acb_configure_block_place.php

--- src/Helper/acb_configure_block_place.php
+++ src/Helper/acb_configure_block_place.php
@@ -4,7 +4,7 @@
  * @file acb_configure_block_place.php
  */
 module_load_include('php', 'acb', 'src/Form/acb_layout_form');
-
+module_load_include('php','acb','src/Helper/acb_load_block');
 
 function acb_node_configure_block($object) {
 
