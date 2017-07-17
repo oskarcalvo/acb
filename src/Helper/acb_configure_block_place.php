e5fee4496c11a4710b4c3882681eaf372aca8a91
Oskar Calvo <calvo.oskar@gmail.com>
2017-07-17 10:41:43 +0200

    Add the required params to the function that load the form in the node page.

--- src/Helper/acb_configure_block_place.php
+++ src/Helper/acb_configure_block_place.php
@@ -6,8 +6,8 @@
 module_load_include('php', 'acb', 'src/Form/acb_layout_form');
 module_load_include('php','acb','src/Helper/acb_load_block');
 
-function acb_node_configure_block($object) {
-
-  return drupal_get_form('acb_layout_form');
+function acb_node_configure_block($type, $object) {
+  $path = $type.'/'.$object->nid;
+  return drupal_get_form('acb_layout_form',$path, $type);
     
 }
