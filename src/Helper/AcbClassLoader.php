7367f5534cba7eb742f0da0ec8ee017cc64e65aa
Oskar Calvo <calvo.oskar@gmail.com>
2017-07-24 09:45:38 +0200

    Add file AcbClassLoader to prepare a factory loader class.

---
+++ src/Helper/AcbClassLoader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\acb\Helper;
+
+class AcbClassLoader {
+	private $type;
+	private $module;
+	private $name;
+	
+	public function __construct($type, $module, $name) {
+		
+	}
+}
