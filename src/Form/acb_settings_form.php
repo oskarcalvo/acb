b2ef8285bc91e2129096e2f1cafee75784a354f3
Oskar Calvo <calvo.oskar@gmail.com>
2017-07-31 08:59:50 +0200

    Update acb_list_modules default value.

--- src/Form/acb_settings_form.php
+++ src/Form/acb_settings_form.php
@@ -13,7 +13,7 @@ function acb_settings_form() {
 		'#type' => 'checkboxes',
 		'#title' => t('Modules defining blocks that can be referenced'),
 		'#options' => AcbLoadBlock::list_of_modules_with_blocks(),
-		// '#default_value' => new AcbLoadBlock::waduis(),
+		'#default_value' => variable_get('acb_list_modules', NULL),
 		'#description' => t('If no modules are selected, blocks from all modules will be available.'),
 	];
 	
