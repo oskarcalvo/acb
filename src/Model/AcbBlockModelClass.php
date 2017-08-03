760867e5f2c42bbae1c2430516ef828857e4a278
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-03 17:32:40 +0200

    Add the metho list_of_blocks to recover all the blocks from the selected modules.

--- src/Model/AcbBlockModelClass.php
+++ src/Model/AcbBlockModelClass.php
@@ -4,8 +4,16 @@ namespace Drupal\acb\Model;
 
 class AcbBlockModelClass {
 	
-	static public function list_of_blocks($list_of_modules) {
-	
-	
+	static public function list_of_blocks(array $list_of_modules, $id ) {
+		$all_block_infos = &drupal_static(__FUNCTION__, array());
+		if(!isset($all_block_infos[$i])) {
+			foreach ($list_of_modules as $module) {
+				$blocks = module_invoke($module, 'block_info');
+				foreach ($blocks as $delta => $info) {
+					$moddelta = $module . ':' . $delta;
+					$all_block_infos[$id][$moddelta] = _blockreference_block($module, $delta, $info);
+				}
+			}
+		}
 	}
 }
