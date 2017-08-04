0bda41a87277e524bd30fae537cb53b1593662eb
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-04 10:48:20 +0200

    Update the method list_of_blocks to remove old code.

--- src/Model/AcbBlockModelClass.php
+++ src/Model/AcbBlockModelClass.php
@@ -6,10 +6,10 @@ use Drupal\acb\Helper\AcbLoadBlock;
 
 class AcbBlockModelClass {
 	
-	static public function list_of_blocks(array $list_of_modules, $id ) {
-		$list_of_blocks = &drupal_static(__FUNCTION__, array());
-		unset($list_of_blocks);
-		if(!isset($list_of_blocks)) {
+	static public function list_of_blocks(array $list_of_modules) {
+		$blocks = &drupal_static(__FUNCTION__, array());
+		
+		if(!isset($blocks)) {
 			foreach ($list_of_modules as $module) {
 				$blocks[$module] = module_invoke($module, 'block_info');
 			}
