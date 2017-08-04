2d3e100b69f8c8d9cbf09fade4a0218a7a380326
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-04 10:47:15 +0200

    Update the method list_of_blocks to retrieve only the required information.

--- src/Model/AcbBlockModelClass.php
+++ src/Model/AcbBlockModelClass.php
@@ -2,18 +2,18 @@
 
 namespace Drupal\acb\Model;
 
+use Drupal\acb\Helper\AcbLoadBlock;
+
 class AcbBlockModelClass {
 	
 	static public function list_of_blocks(array $list_of_modules, $id ) {
-		$all_block_infos = &drupal_static(__FUNCTION__, array());
-		if(!isset($all_block_infos[$i])) {
+		$list_of_blocks = &drupal_static(__FUNCTION__, array());
+		unset($list_of_blocks);
+		if(!isset($list_of_blocks)) {
 			foreach ($list_of_modules as $module) {
-				$blocks = module_invoke($module, 'block_info');
-				foreach ($blocks as $delta => $info) {
-					$moddelta = $module . ':' . $delta;
-					$all_block_infos[$id][$moddelta] = _blockreference_block($module, $delta, $info);
-				}
+				$blocks[$module] = module_invoke($module, 'block_info');
 			}
 		}
+		return $blocks;
 	}
 }
