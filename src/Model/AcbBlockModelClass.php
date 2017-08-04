89e339c4b844b3703c5f7fe157de39d87aa71127
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-04 11:37:29 +0200

    Update the method list_of_blocks.

--- src/Model/AcbBlockModelClass.php
+++ src/Model/AcbBlockModelClass.php
@@ -7,13 +7,11 @@ use Drupal\acb\Helper\AcbLoadBlock;
 class AcbBlockModelClass {
 	
 	static public function list_of_blocks(array $list_of_modules) {
-		$blocks = &drupal_static(__FUNCTION__, array());
 		
-		if(!isset($blocks)) {
-			foreach ($list_of_modules as $module) {
-				$blocks[$module] = module_invoke($module, 'block_info');
-			}
+		foreach ($list_of_modules as $module) {
+			$blocks[$module] = module_invoke($module, 'block_info');
 		}
+		
 		return $blocks;
 	}
 }
