e9d7fedc05ec9569e8e5b1704a88060cc743d5c4
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-08 10:03:06 +0200

    Create method build_output. Retrieve the method list_of_modules_with_blocks. Update the method selectd mdoules, now it retrieve a list of modules or an array with [all => ALL]. Update code to output the json with the list of blocks.

--- src/Helper/AcbLoadBlock.php
+++ src/Helper/AcbLoadBlock.php
@@ -10,10 +10,36 @@ use Drupal\acb\Model\AcbBlockModelClass;
 class AcbLoadBlock {
 	
 	/**
+	 * This method is a copy from the function		 +	 * @return null|array
+	 * _blockreference_get_block_modules() from blockreference module.
+	 *
+	 * @return array|bool|mixed
+	 */
+	public static function list_of_modules_with_blocks() {
+		$modules = &drupal_static(__FUNCTION__);
+	
+		if (!$modules) {
+			// Get modules that define blocks.
+			$modules = module_implements('block_info');
+			$modules = array_flip($modules);
+		
+				// And get their pretty names.
+			$all_modules = system_list('module_enabled');
+			foreach ($modules as $machine_name => $foo) {
+				$modules[$machine_name] = @$all_modules[$machine_name]->info['name'] ?: $machine_name;
+			}
+  
+  		natcasesort($modules);
+ 		}
+ 
+ 		return $modules;
+ 	}
+	
+	/**
 	 * @return null|array
 	 */
 	public static function selected_modules() {
-		return variable_get('acb_list_modules', NULL);
+		return variable_get('acb_list_modules', ['all' => 'ALL']);
 	}
 	
 	/**
@@ -30,12 +56,31 @@ class AcbLoadBlock {
 		$blocks = self::build_blocks_array($blocks);
 		
 		// filter by search string
-		if($string) {
-			$blocks = self::filter_blocks($string,$blocks);
-		}
+		$blocks = self::filter_blocks($string,$blocks);
+		
+		// Build output
+		$blocks = self::build_output($blocks);
+		
+		drupal_json_output($blocks);
 	}
 	
-	
+	/**
+	 * @param $blocks
+	 * @return mixed
+	 */
+	private static function build_output(array $blocks) {
+		
+		foreach ($blocks as $block) {
+			$key = $block->info .'['. $block->module .':'.$block->delta .']';
+			$return_block[$key] = $block->info;
+		}
+		return $return_block;
+	}
+	/**
+	 * @param $search_string
+	 * @param $blocks
+	 * @return mixed
+	 */
 	public static function filter_blocks($search_string,$blocks){
 		$check_string = drupal_strtolower($search_string);
 		foreach ($blocks as $moddelta => $block) {
