77a1c39df7194c31c8f2a0683fe0be5e511ba6e4
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-08 10:06:49 +0200

    Update variable order.

--- src/Model/AcbModelInterface.php
+++ src/Model/AcbModelInterface.php
@@ -6,6 +6,6 @@ interface AcbModelInterface {
 	public function load_by_id($id);
 	public function load_by_url($url);
 	public function save($url, array $data);
-	public function update($id, $url, array $data);
+	public function update($url, array $data, $id);
 	public function delete($id);
 }
