2d4bad3ed13f645ff55cf597ffd4b8fd6bd8be85
Oskar Calvo <calvo.oskar@gmail.com>
2017-08-08 10:03:54 +0200

    Add an if in list_of_blocks to check if all are selected or not.

--- src/Model/AcbBlockModelClass.php
+++ src/Model/AcbBlockModelClass.php
@@ -8,6 +8,10 @@ class AcbBlockModelClass {
 	
 	static public function list_of_blocks(array $list_of_modules) {
 		
+		if(isset($list_of_modules['all'])){
+			$list_of_modules = AcbLoadBlock::list_of_modules_with_blocks();
+		}
+		
 		foreach ($list_of_modules as $module) {
 			$blocks[$module] = module_invoke($module, 'block_info');
 		}
